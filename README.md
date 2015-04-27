a46aa0168a9eaf606f1df892c7104ffc042b2715
deanjones <deanjones@gmail.com>
2015-04-26 23:20:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Ahome Tooling Server Hazelcast
+======
+
+Ahome Tooling Server Hazelcast is a package containing basic server Hazelcast support.
+
+https://github.com/ahome-it/ahome-tooling-server-hazelcast
+
+```xml
+<repositories>
+  <repository>
+  <id>sonatype</id>
+  <url>https://oss.sonatype.org/content/repositories/snapshots/</url>
+  </repository>
+</repositories>
+
+<dependency>
+  <groupId>com.ahome-it</groupId>
+  <artifactId>ahome-tooling-server-hazelcast</artifactId>
+  <version>1.0.0-SNAPSHOT</version>
+</dependency>
+```
+
+License
+
+Ahome Tooling Common is released under version 2.0 of the Apache License.
+
+http://www.apache.org/licenses/LICENSE-2.0.html
---
+++ src/main/groovy/com/ait/tooling/server/hazelcast/support/spring/HazelcastContextInstance.java
@@ -0,0 +1,100 @@
+/*
+   Copyright (c) 2014,2015 Ahome' Innovation Technologies. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.ait.tooling.server.hazelcast.support.spring;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.ait.tooling.server.core.jmx.management.IServerManager;
+import com.ait.tooling.server.core.security.IAuthorizationProvider;
+import com.ait.tooling.server.core.support.spring.IPropertiesProvider;
+import com.ait.tooling.server.core.support.spring.IServerContext;
+import com.ait.tooling.server.core.support.spring.ServerContextInstance;
+
+public final class HazelcastContextInstance implements IHazelcastContext
+{
+    private static final HazelcastContextInstance INSTANCE = new HazelcastContextInstance();
+
+    public static final HazelcastContextInstance get()
+    {
+        return INSTANCE;
+    }
+
+    private HazelcastContextInstance()
+    {
+    }
+
+    @Override
+    public IServerContext getServerContext()
+    {
+        return ServerContextInstance.get();
+    }
+
+    @Override
+    public WebApplicationContext getApplicationContext()
+    {
+        return getServerContext().getApplicationContext();
+    }
+
+    @Override
+    public Environment getEnvironment()
+    {
+        return getServerContext().getEnvironment();
+    }
+
+    @Override
+    public <T> T getBean(final String name, final Class<T> type)
+    {
+        return getServerContext().getBean(name, type);
+    }
+
+    @Override
+    public IPropertiesProvider getPropertiesProvider()
+    {
+        return getServerContext().getPropertiesProvider();
+    }
+
+    @Override
+    public String getPropertyByName(final String name)
+    {
+        return getServerContext().getPropertyByName(name);
+    }
+
+    @Override
+    public String getPropertyByName(final String name, final String otherwise)
+    {
+        return getServerContext().getPropertyByName(name, otherwise);
+    }
+
+    @Override
+    public IAuthorizationProvider getAuthorizationProvider()
+    {
+        return getServerContext().getAuthorizationProvider();
+    }
+
+    @Override
+    public Iterable<String> getPrincipalsKeys()
+    {
+        return getServerContext().getPrincipalsKeys();
+    }
+
+    @Override
+    public IServerManager getServerManager()
+    {
+        return getServerContext().getServerManager();
+    }
+}
